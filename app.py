4ea980ca578c08f5e4a3573b423128ed238a959a
Mudit <muditgupta1711@gmail.com>
2024-10-17 23:39:18 +0530

    random changes

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.set_page_config(page_title="Whatsapp Chat Analyzer", layout="wide")
 
-st.title("WhatsApp Chat Analysis🔎")
-st.write("*Made by Mudit Gupta*")
+st.sidebar.title("WhatsApp Chat Analysis🔎")
+st.sidebar.write("*Made by Mudit Gupta*")  # Move this line to the sidebar
